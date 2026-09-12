e581123372376b279b9cc215944e5320f9f3a4b9
Ace <51465755+AceVikings@users.noreply.github.com>
2026-09-13 03:45:54 +0530

    fix: make trade links portable on static hosting

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import { AppHeader } from "./components/AppHeader";
 import { DecisionPage } from "./pages/DecisionPage";
@@ -6,13 +6,13 @@ import { HomePage } from "./pages/HomePage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppHeader />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/trades/:tradeDigest" element={<DecisionPage />} />
         <Route path="*" element={<DecisionPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
